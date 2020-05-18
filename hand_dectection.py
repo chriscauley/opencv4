51757eacf0a4e59a51a99976744ed0f80fa425c7
chriscauley <chris@lablackey.com>
2020-05-18 17:54:35 +0000

    hand writting

--- hand_dectection.py
+++ hand_dectection.py
@@ -91,19 +91,56 @@ def detect_hand(frame, hist):
     return success, return_value
 
 
-def track_object(source, histogram):
-    cap = cv2.VideoCapture(source)
-    while True:
-        _, frame = cap.read()
-        closing, masked, segment_thresh = locate_object(frame, histogram)
-        cv2.imshow("Closing", closing)
-        cv2.imshow("Masked", masked)
-        cv2.imshow("Segment_Thresh", segment_thresh)
-        key = cv2.waitKey(10)
-        if key == ord('q'):
-            break
+def extract_fingertips(hand):
+    cnt = hand["contours"]
+    points = []
+    hull = cv2.convexHull(cnt, returnPoints=False)
+    defects = cv2.convexityDefects(cnt, hull)
+
+    # get all the "end points" using the defects and contours
+    for i in range(defects.shape[0]):
+        s, e, f, d = defects[i, 0]
+        end = tuple(cnt[e][0])
+        points.append(end)
+
+    # filter out the points which are too close to each other
+    filtered = filter_points(points, 50)
+
+    # sort the fingertips in order of increasing value of the y coordinate
+    filtered.sort(key=lambda point: point[1])
+
+    # return the fingertips, at most 5.
+    return [pt for idx, pt in zip(range(5), filtered)]
+
+
+def filter_points(points, filterValue):
+    for i in range(len(points)):
+        for j in range(i + 1, len(points)):
+            if points[i] and points[j] and dist(points[i], points[j]) < filterValue:
+                points[j] = None
+    filtered = []
+    for point in points:
+        if point is not None:
+            filtered.append(point)
+    return filtered
+
+
+def dist(a, b):
+    return math.sqrt((a[0] - b[0])**2 + (b[1] - a[1])**2)
+
+
+def plot(frame, points):
+    radius = 5
+    colour = (0, 0, 255)
+    thickness = -1
+    for point in points:
+        cv2.circle(frame, point, radius, colour, thickness)
+
 
 if __name__ == "__main__":
+    screen = np.zeros((600, 1000))
+    curr = None
+    prev = None
     cap = cv2.VideoCapture(0)
     try:
         hist = np.load(HIST_FILE)
@@ -115,16 +152,35 @@ if __name__ == "__main__":
         if not ret:
             break
         frame = cv2.flip(frame, 1)
-        success, hand = detect_hand(frame, hist)
+        hand_detected, hand = detect_hand(frame, hist)
 
-        for key, value in hand.items():
-            if key == 'contours':
-                continue
-            cv2.imshow(key, value)
+        if hand_detected:
+            hand_image = hand["boundaries"]
+
+            fingertips = extract_fingertips(hand)
+            plot(hand_image, fingertips)
+
+            prev = curr
+            curr = fingertips[0]
+
+            if prev and curr:
+                cv2.line(screen, prev, curr, (255, 255, 255), 5)
+
+            cv2.imshow("Drawing", screen)
+            cv2.imshow("HD", hand_image)
+
+        else:
+            cv2.imshow("HD", frame)
+        # for key, value in hand.items():
+        #     if key == 'contours':
+        #         continue
+        #     cv2.imshow(key, value)
 
         key = cv2.waitKey(10)
         if key == ord('q'):
             break
+        if key == ord('c'):
+            screen = np.zeros((600, 1000))
         if key == ord('h'):
             cv2.destroyAllWindows()
             hist = capture_histogram(cap)
